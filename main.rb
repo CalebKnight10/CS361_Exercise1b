f7cdda3c5e57d84e21e78b49db3ca4853fdde1a3
Caleb Knight <knightca@oregonstate.edu>
2021-10-03 14:34:06 -0700

    Changed code to fit Martins example

---
+++ main.rb
@@ -0,0 +1,26 @@
+class Bike
+  # ...
+end
+
+class RedBicycle < Bike
+
+  POUNDS = 10
+
+  def initialize(height, weight, color)
+    @height = height
+    @weight = weight
+    @color = color
+  end
+
+  def get_color
+    @color
+  end
+
+  def get_height
+    @height
+  end
+
+  def set_weight
+    @weight -= POUNDS
+  end
+end
